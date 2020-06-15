5ced55fd85741ac3f720bd9f1219a4220c52759a
DrunkDuckling <soerensen5492@gmail.com>
2020-06-15 16:59:56 +0200

    Last push
    GOD i hope i pass

--- app/src/main/java/com/example/mdsdproject/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/mdsdproject/fragments/SettingsFragment.java
@@ -17,22 +17,9 @@ import android.widget.Toast;
 
 import com.example.mdsdproject.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1, mSpinnerText;
-    private String mParam2;
-
+    private String mSpinnerText;
     private Double number;
     private EditText editText;
 
@@ -40,32 +27,9 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -90,7 +54,6 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         mSpinnerText = parent.getItemAtPosition(position).toString();
-        //Toast.makeText(parent.getContext(), mSpinnerText, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -102,16 +65,14 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button:
-                String failsafe = editText.getText().toString();
-                if(failsafe.matches("")){
+                String failSafe = editText.getText().toString();
+                if(failSafe.matches("")){
                     Toast.makeText(getActivity().getApplicationContext(), "Please specify number", Toast.LENGTH_SHORT).show();
                 }else{
                     FragmentTransaction transaction = getFragmentManager().beginTransaction();
                     MapsFragment mapsFragment = new MapsFragment();
 
-                    //number = Integer.parseInt(editText.getText().toString());
                     number = Double.parseDouble(editText.getText().toString());
-                    //Toast.makeText(getActivity().getApplicationContext(), "BUTTON" + mSpinnerText.toString() + number.toString(), Toast.LENGTH_SHORT).show();
                     Bundle bundle = new Bundle();
                     bundle.putString("location", mSpinnerText);
                     bundle.putDouble("radius", number);
